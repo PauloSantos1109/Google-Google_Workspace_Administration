f309a0e597d20fe8a94dc58828e30954fdfd56bc
paulo santos <paulo.santos@gpnet.com.br>
2023-12-12 10:47:18 -0300

    💻 <> Code: Assigning identifiers in spreadsheets

--- Administering_the_Organizational_Units/Atribuir massivo.mjs
+++ Administering_the_Organizational_Units/Atribuir massivo.mjs
@@ -1,8 +1,8 @@
 function moveDevices() {
     var cb = {
      customerId: 'my_customer',
-     orgUnitPath:'/Retenção/Teste (Paulo Santos)/Scratch Day',
-     deviceIds: '8a28ea42-028a-469e-adbf-e6ca7afe5a1c'
+     orgUnitPath:'/Organizational_unit_name',
+     deviceIds: 'SSID_TO_DEVICE'
     };  
      var moveCb = AdminDirectory.Chromeosdevices.moveDevicesToOu(cb,cb.customerId,cb.orgUnitPath);
    
